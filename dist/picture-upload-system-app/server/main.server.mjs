5dd779a99a1ab1b7175993299a810f76b29426f4
Canfer Ulaş Şahin <ulssahn@outlook.com>
2024-08-16 01:46:18 +0300

    fist commit

--- dist/picture-upload-system-app/server/main.server.mjs
+++ dist/picture-upload-system-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RGDEOXWV.mjs";import"./chunk-RSNZBVMM.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
+import{a}from"./chunk-4EOM6VAA.mjs";import"./chunk-RSNZBVMM.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
-    production: false,
+    production: true,
     firebase: {
-        apiKey: "AIzaSyCQ9ofPSzRK9RFaB1z8INvIQAB5HZXQk8Q",
-        authDomain: "picture-upload-system.firebaseapp.com",
-        projectId: "picture-upload-system",
-        storageBucket: "picture-upload-system.appspot.com",
-        messagingSenderId: "1015369941222",
-        appId: "1:1015369941222:web:b2b8dc11b7ba294e46bb27"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
     }
   };
