26629b87e32c9f6e79fcfcbabf7fc836f59410a1
Canfer Ulaş Şahin <ulssahn@outlook.com>
2024-08-16 22:01:48 +0300

    edit file-upload-component.tt

--- dist/picture-upload-system-app/server/main.server.mjs
+++ dist/picture-upload-system-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4EOM6VAA.mjs";import"./chunk-RSNZBVMM.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
+import{a}from"./chunk-TIVZ6ZL4.mjs";import"./chunk-VQ5Q4UXO.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -11,6 +11,7 @@ export class FileUploadComponent {
   selectedFiles: File[] = [];
   uploadProgress: number = 0;
   downloadURL: string | undefined;
+  isUploading: boolean = false; 
 
   constructor(private storage: AngularFireStorage) {}
 
@@ -20,10 +21,14 @@ export class FileUploadComponent {
 
   onUpload() {
     if (this.selectedFiles.length > 0) {
+      this.isUploading = true;
+      let selectedFilesSize = this.selectedFiles.length;
+      let uploadedSizeFile = 0; 
       this.selectedFiles.forEach(file => {
         const filePath = `uploads/${Date.now()}_${file.name}`;
         const fileRef = this.storage.ref(filePath);
         const task = this.storage.upload(filePath, file);
+        
 
         task.percentageChanges().subscribe(progress => {
           this.uploadProgress = progress || 0;
@@ -35,11 +40,22 @@ export class FileUploadComponent {
             fileRef.getDownloadURL().subscribe(url => {
               this.downloadURL = url;
               console.log('File available at', this.downloadURL);
-              alert("Yükleme Başarılı. ♥ Teşekkür Ederiz ♥")
+              uploadedSizeFile++;
+
+
             });
           })
         ).subscribe();
+        
+      
       });
+      let intervalId = setInterval(()=> {
+        if (uploadedSizeFile == selectedFilesSize) {
+          clearInterval(intervalId);
+          alert("Yükleme Başarılı. ♥ Teşekkür Ederiz ♥");
+          window.location.reload();
+        }
+      },1000)
     }
   }
 }
