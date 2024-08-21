979fb8f321eb279e06adc8fc238692ec9c4fca6f
Canfer Ulaş Şahin <ulssahn@outlook.com>
2024-08-22 02:50:02 +0300

    Add progress bar

--- dist/picture-upload-system-app/server/main.server.mjs
+++ dist/picture-upload-system-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TIVZ6ZL4.mjs";import"./chunk-VQ5Q4UXO.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
+import{a}from"./chunk-337ZIQQY.mjs";import"./chunk-QKB2VVO6.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -9,53 +9,50 @@ import { finalize } from 'rxjs/operators';
 })
 export class FileUploadComponent {
   selectedFiles: File[] = [];
-  uploadProgress: number = 0;
-  downloadURL: string | undefined;
-  isUploading: boolean = false; 
+  uploadProgress: number[] = [];
+  downloadURLs: string[] = [];
+  isUploading: boolean = false;
 
   constructor(private storage: AngularFireStorage) {}
 
   onFileSelected(event: any) {
     this.selectedFiles = Array.from(event.target.files);
+    this.uploadProgress = new Array(this.selectedFiles.length).fill(0); // Yükleme ilerlemesini sıfırlayın
   }
 
   onUpload() {
     if (this.selectedFiles.length > 0) {
       this.isUploading = true;
-      let selectedFilesSize = this.selectedFiles.length;
-      let uploadedSizeFile = 0; 
-      this.selectedFiles.forEach(file => {
+      let uploadedFilesCount = 0;
+
+      this.selectedFiles.forEach((file, index) => {
         const filePath = `uploads/${Date.now()}_${file.name}`;
         const fileRef = this.storage.ref(filePath);
         const task = this.storage.upload(filePath, file);
-        
 
+        // Yükleme yüzdesini takip et
         task.percentageChanges().subscribe(progress => {
-          this.uploadProgress = progress || 0;
-          console.log(`File is ${this.uploadProgress}% uploaded.`);
+          this.uploadProgress[index] = progress || 0;
+          console.log(`File ${file.name} is ${this.uploadProgress[index]}% uploaded.`);
         });
 
         task.snapshotChanges().pipe(
           finalize(() => {
             fileRef.getDownloadURL().subscribe(url => {
-              this.downloadURL = url;
-              console.log('File available at', this.downloadURL);
-              uploadedSizeFile++;
+              this.downloadURLs[index] = url;
+              console.log('File available at', url);
+              uploadedFilesCount++;
 
-
+              // Tüm dosyalar yüklendiğinde
+              if (uploadedFilesCount === this.selectedFiles.length) {
+                this.isUploading = false;
+                alert("Yükleme Başarılı. ♥ Teşekkür Ederiz ♥");
+                window.location.reload();
+              }
             });
           })
         ).subscribe();
-        
-      
       });
-      let intervalId = setInterval(()=> {
-        if (uploadedSizeFile == selectedFilesSize) {
-          clearInterval(intervalId);
-          alert("Yükleme Başarılı. ♥ Teşekkür Ederiz ♥");
-          window.location.reload();
-        }
-      },1000)
     }
   }
 }
